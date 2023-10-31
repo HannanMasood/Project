864489979ba9149dfd7b747223114bc60f6cb99b
HannanMasood <149540068+HannanMasood@users.noreply.github.com>
2023-10-31 14:58:33 -0700

    Add files via upload

---
+++ signup_page.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:project/pages/login_page.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 60),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Signup",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade400)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade300)))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Create password',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade400)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade300)))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Confirm password',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade400)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade300)))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    color: Colors.blue.shade700,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Center(
+                  child: Text(
+                    'Signup',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return Loginpage();
+                      },
+                    )),
+                    child: Text(
+                      'Login?',
+                      style:
+                          TextStyle(color: Colors.blue.shade700, fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 60),
+                    child: Divider(
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                  Text(
+                    'Or',
+                    style: TextStyle(color: Colors.grey.shade600, fontSize: 18),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, left: 60),
+                    child: Divider(
+                      color: Colors.grey.shade900,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    color: Colors.blue.shade900,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Center(
+                        child: Image.asset(
+                          'lib/images/facebook.png',
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Text(
+                      'Login with Facebook',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade400, width: 2),
+                    //color: Colors.blue.shade900,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Row(
+                  children: [
+                    Center(
+                      child: Image.asset(
+                        'lib/images/Goog.png',
+                        height: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 70,
+                    ),
+                    Text(
+                      'Login with Google',
+                      style:
+                          TextStyle(color: Colors.grey.shade600, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
